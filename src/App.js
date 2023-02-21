742718c861193f8fd1c350081f81e3211f4081ad
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-02-21 21:55:47 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>First Commit</h1>
     </div>
   );
 }
 
 export default App;
+
+// Report for
+// Jeremy Robson
+
+// Daily
+// Weekly
+// Monthly
+
+// Work
+// 5hrs <!-- daily -->
+// Previous - 7hrs <!-- daily -->
+// 32hrs <!-- weekly -->
+// Previous - 36hrs <!-- weekly -->
+// 103hrs <!-- monthly -->
+// Previous - 128hrs <!-- monthly -->
+
+// Play
+// 1hr <!-- daily -->
+// Previous - 2hrs <!-- daily -->
+// 10hrs <!-- weekly -->
+// Previous - 8hrs <!-- weekly -->
+// 23hrs <!-- monthly -->
+// Previous - 29hrs <!-- monthly -->
+
+// Study
+// 0hrs <!-- daily -->
+// Previous - 1hr <!-- daily -->
+// 4hrs <!-- weekly -->
+// Previous - 7hrs <!-- weekly -->
+// 13hrs <!-- monthly -->
+// Previous - 19hrs <!-- monthly -->
+
+// Exercise
+// 1hr <!-- daily -->
+// Previous - 1hr <!-- daily -->
+// 4hrs <!-- weekly -->
+// Previous - 5hrs <!-- weekly -->
+// 11hrs <!-- monthly -->
+// Previous - 18hrs <!-- monthly -->
+
+// Social
+// 1hr <!-- daily -->
+// Previous - 3hrs <!-- daily -->
+// 5hrs <!-- weekly -->
+// Previous - 10hrs <!-- weekly -->
+// 21hrs <!-- monthly -->
+// Previous - 23hrs <!-- monthly -->
+
+// Self Care
+// 0hrs <!-- daily -->
+// Previous - 1hr <!-- daily -->
+// 2hrs <!-- weekly -->
+// Previous - 2hrs <!-- weekly -->
+// 7hrs <!-- monthly -->
+// Previous - 11hrs <!-- monthly -->
