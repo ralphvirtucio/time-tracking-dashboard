66a27d1ed0eebfc3bb2ad5dc78c8014d9a32058c
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-02-23 00:13:10 +0800

    Feat: Dynamic rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,35 @@ import { Ellipsis } from './Ellipsis';
 import { Icon } from './Icon';
 
 export const Card = ({ title, timeframe }) => {
+  let cardClass = '';
+  switch (title.toLowerCase()) {
+    case 'work':
+      cardClass = 'work__container';
+      break;
+    case 'play':
+      cardClass = 'play__container';
+      break;
+    case 'study':
+      cardClass = 'study__container';
+      break;
+    case 'exercise':
+      cardClass = 'exercise__container';
+      break;
+    case 'social':
+      cardClass = 'social__container';
+      break;
+    case 'self care':
+      cardClass = 'selfcare__container';
+      break;
+    default:
+  }
+
   return (
-    <div className={classes.card__container}>
-      {/* <div className={classes['card__photo-container']}> */}
-      {/* <img src={workImg} alt='Play' className={classes.card__photo} /> */}
-      <Icon iconName={title.toLowerCase()} />
-      {/* </div> */}
+    <div className={classes[`${cardClass}`]}>
+      <div className={classes['card__photo-container']}>
+        {/* <img src={workImg} alt='Play' className={classes.card__photo} /> */}
+        <Icon iconName={title.toLowerCase()} />
+      </div>
       <div className={classes.card__content}>
         <div className={classes['card__row']}>
           <h2 className={classes.card__title}>{title}</h2>
