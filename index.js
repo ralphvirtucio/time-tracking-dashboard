b964a27faf2c26720b4bd791dffb049de777d168
ralphvirtucio <ralphvirtucio3@gmail.com>
2024-08-05 22:38:52 +0800

    Features: Dynamic cards and javascript interactivity

--- index.js
+++ index.js
@@ -1,113 +1,118 @@
-const tabList = document.querySelector('[role=tablist]')
+const tabList = document.querySelector('[role=tablist]');
+const cardList = document.querySelector('.cards');
 const tabs = document.querySelectorAll('[role=tab]');
-const cards = document.querySelector('.cards')
 
-const cardHTML = (data, selectedTimeFrame) => {
-  const {title, timeframes} = data
-  const titleLowerCase = title.toLowerCase()
-  const cleanedTitle = titleLowerCase.replace(' ', '');
-  const cardClass = `card ${cleanedTitle}`
-  const iconURL = `${titleLowerCase.trim().replace(' ', '-')}`
-  console.log(selectedTimeFrame)
-  const html = 
-              `<div class="img">
-                <img src="./images/icon-${iconURL}.svg" alt="">
+async function init() {
+  const data = await fetchData();
+
+  data.forEach(createTimeData);
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click',(e) =>  switchTabs(e, data));
+  });
+}
+
+async function fetchData() {
+  try {
+    const response = await fetch('/data.json');
+
+    if (!response.ok) {
+      throw new Error(`Something went wrong: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(`HTTP STATUS: ${error.message}`);
+  }
+}
+
+const createTimeData = (data) => {
+  const { title, timeframes } = data;
+
+  const card = document.createElement('li');
+  const cardClassTitle = title.toLowerCase().replace(' ', '');
+  card.classList.add('card');
+  card.classList.add(cardClassTitle);
+
+  card.innerHTML = createCardElement(title, timeframes, 'daily');
+
+  cardList.appendChild(card);
+};
+
+
+const createCardElement = (title, timeFrame, selectedTimeFrame) => {
+  const titleLowerCase = title.toLowerCase();
+  const cleanedTitle = `${titleLowerCase.trim().replace(' ', '-')}`;
+
+  const html = `<div class="img">
+                <img src="./images/icon-${cleanedTitle}.svg" alt="">
               </div>
               <div class="card__content">
               <div class="card__header">
                 <h2 class="card__heading">
-                   <a href="#${titleLowerCase}">${title}</a>
+                   <a href="#${cleanedTitle}">${title}</a>
                 </h2>
                 <button aria-label="More info" class="more-info__btn">
                   <svg aria-hidden="true" focusable="false" width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
                 </button>
               </div>
-              <div class="card__body daily">
+              <div class="card__body">
                 <h3 class="current">
-                  ${timeframes[selectedTimeFrame].current}hrs
+                  ${timeFrame[selectedTimeFrame].current}hrs
                 </h3>
-                <p class="previous">
-                  <span>Last Week</span>
+                <p class="previous__time-info">
+                  <span class="previous__time-label">Yesterday</span>
                   -
-                  <span>${timeframes[selectedTimeFrame].previous}</span>
+                  <span class="previous">${timeFrame[selectedTimeFrame].previous}hrs</span>
                 </p>
               </div>
             </div>
-              `
+              `;
 
   return html;
-}
+};
 
-async function fetchData() {
-  try {
-    const response = await fetch("data.json");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    } 
+const switchTabs = (e, data) => {
+  e.preventDefault();
+  const selectedTab = e.currentTarget;
+  const currentTab = tabList.querySelector('[aria-selected]');
+  if (selectedTab !== currentTab) {
+    selectedTab.focus();
 
-    const data = await response.json();
+    selectedTab.removeAttribute('tabindex');
 
-    
-    return data
-  } catch (error) {
-    console.error("Fetch error:", error);
-  }
-}
+    selectedTab.setAttribute('aria-selected', 'true');
+    currentTab.removeAttribute('aria-selected');
+    currentTab.setAttribute('tabindex', '-1');
 
+    data.forEach((res, i) => {
+      const current = document.querySelectorAll('.current')
+      const previous = document.querySelectorAll('.previous')
+      const selectedTabValue = selectedTab.textContent.toLowerCase()
+      const selectedTimeFrame = res.timeframes[selectedTabValue]
+      
+      const previousLabel = document.querySelectorAll('.previous__time-label')
+      let previousLabelValue = ''
 
-async function init() {
-  const data = await fetchData()
-
-  data.forEach(res => {
-    const card = document.createElement("li")
-    const title = res.title.replace(' ', '').toLowerCase()
-    card.classList.add('card')
-    card.classList.add(title)
-    
-    
-    card.innerHTML = cardHTML(res, 'daily')
-    cards.appendChild(card)
-  })
-
-
-  tabs.forEach((tab) => {
-    
-    tab.addEventListener('click', (e) => {
-      e.preventDefault()
-      const selectedTab = e.currentTarget
-      const currentTab = tabList.querySelector('[aria-selected]')
-
-      if(selectedTab !== currentTab) {
-        selectedTab.focus()
-
-      selectedTab.removeAttribute('tabindex')
-
-      selectedTab.setAttribute('aria-selected', 'true')
-      currentTab.removeAttribute('aria-selected')
-      currentTab.setAttribute('tabindex', '-1')
-
-      data.forEach(res => {
-          const card = document.createElement("li")
-          const title = res.title.replace(' ', '').toLowerCase()
-          card.classList.add('card')
-          card.classList.add(title)
-          const selectedTimeFrame = selectedTab.getAttribute('id')
-          card.innerHTML = cardHTML(res, selectedTimeFrame)
-
-          cards.appendChild(card)
-
-      })
-
+      if(selectedTabValue === 'daily') {
+        previousLabelValue = 'Yesterday'
+      } else if (selectedTabValue === 'weekly') {
+        previousLabelValue = 'Last Week'
+      } else {
+        previousLabelValue = 'Last Month'
       }
+
+      previousLabel[i].textContent = previousLabelValue
+
+      current[i].textContent = selectedTimeFrame.current + 'hrs'
+      previous[i].textContent = selectedTimeFrame.previous + 'hrs'
+
+      
     })
-  })
-}
+  }
+};
 
 
-
-
-window.addEventListener('DOMContentLoaded', init)
-
-// tabs.forEach(tab => {
-//   console.log(tab)
-// })
+window.addEventListener('DOMContentLoaded', init);
